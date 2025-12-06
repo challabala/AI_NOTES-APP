9768981a532481cd67e1089152de903df2845d7b
challabala <challabala58@gmail.com>
2025-12-06 17:44:39 +0530

    added url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://ai-notes-app-f6g5.onrender.com',
     withCredentials: true, // sending cookies
 });
 
